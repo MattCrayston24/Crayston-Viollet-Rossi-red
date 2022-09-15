b0e3cf7a311873b5cd6d939cf434154632e984dc
EnzoRss <enzo.rossi@ynov.com>
2022-09-15 17:01:36 +0200

    Change

---
+++ src/Dossier_Package/Menu.go
@@ -0,0 +1,21 @@
+package Dossier_Package
+
+import "fmt"
+
+// Fonction pour définir le menu :
+func Menu() {
+	var nombre_choisi string
+	for {
+		fmt.Print("Si vous voulez accédez a l'inventaire taper 1, \n Si vous voulez accédez aux statistiques de votre personnage taper 2, \n Si vous voulez quitter taper 3 : ")
+		fmt.Scan(&nombre_choisi)
+		if nombre_choisi == "1" {
+			fmt.Println("salut")
+			break
+		} else if nombre_choisi == "2" {
+			fmt.Println("coucou")
+			break
+		} else if nombre_choisi == "3" {
+			break
+		}
+	}
+}
