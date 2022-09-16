a8567964177e28c753e7b5095f19f3dfb0538930
MattCrayston24 <113009631+MattCrayston24@users.noreply.github.com>
2022-09-16 15:26:04 +0200

    Create Marchand.go

---
+++ src/Dossier_Package/Marchand.go
@@ -0,0 +1,14 @@
+func (p *Personnage) Marchand(s string){ 
+	var item_choisi string
+	if len(p.inventaire) < 10{
+		fmt.Println("Potion de bouclier taper 1 \n Potion de vie taper 2 ")	
+		fmt.Scan(&item_choisi)
+		if item_choisi == "1"{
+			p.inventaire = append(p.inventaire, "Potion de bouclier")
+			p.monnaie -= 30
+		} else if item_choisi == "2"{
+			p.inventaire = append(p.inventaire, "Potion de vie")
+			p.monnaie -= 20
+			}
+	}
+}
